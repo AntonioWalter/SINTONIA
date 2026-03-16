cd478b2b36957ec1a639a8afed63282f18b7524b
AntonioWalterDeFusco <defuscoantoniowalter@gmail.com>
2026-03-16 16:32:38 +0100

    fix: autorizzazione vercel

--- webapp/backend/src/main.ts
+++ webapp/backend/src/main.ts
@@ -3,14 +3,13 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const allowedOrigins = process.env.ALLOWED_ORIGINS
-    ? process.env.ALLOWED_ORIGINS.split(',')
-    : ['http://localhost:5173', 'http://localhost:5174'];
-
   app.enableCors({
-    origin: allowedOrigins,
+    origin: [
+      'https://sintonia-inky.vercel.app',
+      'http://localhost:5173',
+      'http://localhost:5174'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
